08b30b3092bf15aa74e171307d6dee12c53c9951
Fred Nielsen <16697069+fredless@users.noreply.github.com>
2021-04-22 10:31:06 -0700

    add processing of multiple `starts_with` as list

--- did_block_finder.py
+++ did_block_finder.py
@@ -39,7 +39,7 @@ STATUS_QUERY_WAIT = 'Querying flowroute for available numbers, this may take a w
 FR_NAPI_URL = 'https://api.flowroute.com/v2/numbers/available'
 FR_NAPI_PAGE = 200
 
-STARTS_WITH = 1408
+STARTS_WITH = [1408]
 MIN_SIZE = 15
 
 # we can also look for interesting digit strings while we hunt for blocks
@@ -71,59 +71,62 @@ def main():
 
     fr_api_session = requests.Session()
 
-    # store list of returned numbers as strings
-    str_num_list = []
-    offset = 0
+    # search list of area codes
+    for starts_with in STARTS_WITH:
 
-    # query numbers one page at a time
-    more_results = True
-    while more_results:
-        print_status(f'{STATUS_QUERY_WAIT}{offset}')
-        response = fr_api_session.get (FR_NAPI_URL,
-                                       auth=fr_auth,
-                                       params={'order_by': 'tn,asc',
-                                               'limit': FR_NAPI_PAGE,
-                                               'offset': offset,
-                                               'starts_with': 1408})
-        if response.status_code == 200:
-            result = response.json()
+        # store list of returned numbers as strings
+        str_num_list = []
+        offset = 0
 
-            # drop each returned phone number into list
-            for record in result['data']:
-                str_num_list.append(record['id'])
+        # query numbers one page at a time
+        more_results = True
+        while more_results:
+            print_status(f'{STATUS_QUERY_WAIT}{offset}')
+            response = fr_api_session.get (FR_NAPI_URL,
+                                        auth=fr_auth,
+                                        params={'order_by': 'tn,asc',
+                                                'limit': FR_NAPI_PAGE,
+                                                'offset': offset,
+                                                'starts_with': starts_with})
+            if response.status_code == 200:
+                result = response.json()
 
-            if 'next' in result['links']:
-                # another page of results awaits
-                offset += FR_NAPI_PAGE
+                # drop each returned phone number into list
+                for record in result['data']:
+                    str_num_list.append(record['id'])
+
+                if 'next' in result['links']:
+                    # another page of results awaits
+                    offset += FR_NAPI_PAGE
+                else:
+                    # we're at the last page
+                    print_status(f'{STATUS_QUERY_WAIT}{FIN}', linefeed=2)
+                    more_results = False
+
             else:
-                # we're at the last page
-                print_status(f'{STATUS_QUERY_WAIT}{FIN}', linefeed=2)
-                more_results = False
-
-        else:
-            # anything other than 200 comes back, error out
-            print(f'ERROR {response.status_code}:{response.json()}')
+                # anything other than 200 comes back, error out
+                print(f'ERROR {response.status_code}:{response.json()}')
 
 
-    # convert list of number strings to integers for searching
-    int_num_list = [int(i) for i in str_num_list]
+        # convert list of number strings to integers for searching
+        int_num_list = [int(i) for i in str_num_list]
 
-    # output list of ranges that meet criteria
-    for _,g in groupby(enumerate(int_num_list),lambda x:x[0]-x[1]):
-        group = map(itemgetter(1), g)
-        group = list(map(int,group))
-        if group[0] + MIN_SIZE - 1 <= group[-1]:
-            how_many = group[-1] - group[0] + 1
-            print(f'Block of {how_many}: {group[0]} - {group[-1]}')
+        # output list of ranges that meet criteria
+        for _,g in groupby(enumerate(int_num_list),lambda x:x[0]-x[1]):
+            group = map(itemgetter(1), g)
+            group = list(map(int,group))
+            if group[0] + MIN_SIZE - 1 <= group[-1]:
+                how_many = group[-1] - group[0] + 1
+                print(f'Block of {how_many}: {group[0]} - {group[-1]}')
 
-    print()
+        print()
 
-    # bonus: looks for interesting strings in numbers queried
-    for number in str_num_list:
-        if any(match in number for match in UNIQUE_DIGITS):
-            print(f'Interesting string in {number}')
+        # bonus: looks for interesting strings in numbers queried
+        for number in str_num_list:
+            if any(match in number for match in UNIQUE_DIGITS):
+                print(f'Interesting string in {number}')
 
-    print()
+        print()
 
 if __name__ == "__main__":
     main()
